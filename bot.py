73333f17bc09574c7e1aff5b6f08a603c9d44dc8
JeF11037 <lev.petryakov@gmail.com>
2021-02-27 13:30:45 +0200

    Add keyboard implementation. Add Keyboard, Text and KeyboardButtonColor imports from vkbottle

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import vkbottle.bot as bot
 from vkbottle.bot import Message
+from vkbottle import Keyboard, Text, KeyboardButtonColor
 import asyncio
 from api import API
 from typing import Optional
@@ -8,6 +9,16 @@ class Bot:
     def __init__(self, token):
         self.bot = bot.Bot(token)
         self.api = API()
+
+        self.keyboard = Keyboard(one_time=False, inline=False) \
+            .add(Text("замены"), color=KeyboardButtonColor.POSITIVE) \
+            .add(Text("консультации"), color=KeyboardButtonColor.POSITIVE) \
+            .row() \
+            .add(Text("убрать клавиатуру"), color=KeyboardButtonColor.NEGATIVE) \
+            .get_json()
+
+        self.clearboard = Keyboard(one_time=True, inline=False) \
+            .get_json()
 
         @self.bot.on.chat_message(text=["-thk?changes <param>"])
         @self.bot.on.private_message(text=["изменения в расписании <param>", "замены <param>"])
@@ -18,13 +29,23 @@ class Bot:
                 changes = self.api.get_changes()
             if changes:
                 await message.answer(changes)
-            await message.answer("Для группы, которую вы указали изменений в расписании нет.")
+            await message.answer("для группы, которую вы указали изменений в расписании нет.")
 
         @self.bot.on.chat_message(text=["-thk?consutltations <teacher>"])
         @self.bot.on.private_message(text=["консультации <teacher>"])
         async def any_message(message: Message):
             changes = self.api.get_consultations()
             await message.answer(changes)
+
+        @self.bot.on.chat_message(text=["-thk?привет", "-thk?хай", "-thk?здравствуй", "-thk?команды", "-thk?клавиатура", "-thk?клава"])
+        @self.bot.on.private_message(text=["привет", "хай", "здравствуй", "команды", "клавиатура", "клава"])
+        async def wrapper(message: Message):
+            await message.answer('список команд', keyboard=self.keyboard)
+
+        @self.bot.on.chat_message(text=["-thk?убрать клавиатуру", "-thk?убрать"])
+        @self.bot.on.private_message(text=["убрать клавиатуру", "убрать"])
+        async def wrapper(message: Message):
+            await message.answer('клавиатура убрана', keyboard=self.clearboard)
 
         asyncio.run(self.bot.run_polling())
 
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from bot import Bot
 import os
 
-token = os.environ["token"]
+token = "28ad13f7d1dacf5542ab985a2e8b4168c2eebc7fdf6a294a9ab8338e7e6c79ce59f3c9cb10a5e67fcceee"
 
 if __name__ == '__main__':
     bot = Bot(token)
