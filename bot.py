2079fd6aa257ef6523581806a8a209b0ff208c4f
JeF11037 <lev.petryakov@gmail.com>
2021-02-27 13:40:23 +0200

    Add new params in decorators

--- bot.py
+++ bot.py
@@ -20,8 +20,8 @@ class Bot:
         self.clearboard = Keyboard(one_time=True, inline=False) \
             .get_json()
 
-        @self.bot.on.chat_message(text=["-thk?changes <param>"])
-        @self.bot.on.private_message(text=["изменения в расписании <param>", "замены <param>"])
+        @self.bot.on.chat_message(text=["-thk?изменения в расписании <param>", "-thk?замены <param>", "-thk?замены"])
+        @self.bot.on.private_message(text=["изменения в расписании <param>", "замены <param>", "замены"])
         async def any_message(message: Message, param: Optional[str] = None):
             if param is not None:
                 changes = self.api.get_changes_by_group(param)
@@ -29,10 +29,13 @@ class Bot:
                 changes = self.api.get_changes()
             if changes:
                 await message.answer(changes)
-            await message.answer("для группы, которую вы указали изменений в расписании нет.")
+            elif param:
+                await message.answer("для группы, которую вы указали изменений в расписании нет.")
+            else:
+                await message.answer("изменений в расписании нет.")
 
-        @self.bot.on.chat_message(text=["-thk?consutltations <teacher>"])
-        @self.bot.on.private_message(text=["консультации <teacher>"])
+        @self.bot.on.chat_message(text=["-thk?consutltations <teacher>", "-thk?консультации"])
+        @self.bot.on.private_message(text=["консультации <teacher>", "консультации"])
         async def any_message(message: Message):
             changes = self.api.get_consultations()
             await message.answer(changes)
