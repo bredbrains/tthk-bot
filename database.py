3d5416e97afee377daae495c4ca9a04dd570db83
Nikolas Laus <bchik3@gmail.com>
2021-03-13 00:06:44 +0200

    Task #34 - Database connection. Task #35 - User requests class. Task #33 - Select all users request. Task #31 - Update user requests. Task #32 - Select user requests.

--- database.py
+++ database.py
@@ -1,11 +1,43 @@
 import pymysql
 import os
 
+from models.user import User
+
+
 class Database:
     def __init__(self):
         self.mysql_connection = {
             "host": os.environ["mysql_host"],
-            "login": os.environ["mysql_login"],
+            "user": os.environ["mysql_user"],
             "password": os.environ["mysql_password"],
             "database": os.environ["mysql_db"]
         }
+        self.connection = pymysql.connect(
+            host=self.mysql_connection["host"],
+            user=self.mysql_connection["user"],
+            password=self.mysql_connection["password"],
+
+        )
+
+    def call_cursor(self):
+        return self.connection.cursor()
+
+
+class UserRequest(User):
+    def __init__(self):
+        self.database = Database()
+        self.cursor = self.database.call_cursor()
+
+    def get_user(self, id) -> User:
+        self.cursor.execute(f"""SELECT vkid, thkruhm, sendStatus FROM users WHERE vkid={id};""")
+        response = self.cursor.fetchone()
+        user = User(response["vkid"], response["thkruhm"], response["sendStatus"])
+        return user
+
+    def get_all_users(self) -> list:
+        users = list()
+        self.cursor.execute("""SELECT vkid, thkruhm, sendStatus FROM users;""")
+        response = self.cursor.fetchall()
+        for user in response:
+            users.append(User(user["vkid"], user["thkruhm"], user["sendStatus"]))
+        return users
