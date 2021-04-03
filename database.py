4ed43844b460c8ef97e8bc5a915d47e075919b2f
Nikolas Laus <bchik3@gmail.com>
2021-04-03 16:25:02 +0300

    Update DB scheme to actual

--- database.py
+++ database.py
@@ -33,14 +33,14 @@ class UserRequest:
         self.cursor = self.database.call_cursor()
 
     def get_user(self, id: int) -> User:
-        self.cursor.execute(f"""SELECT vkid, thkruhm, sendStatus FROM users WHERE vkid={id};""")
+        self.cursor.execute(f"""SELECT vkid, school_group, sender_status FROM users WHERE vkid={id};""")
         response = self.cursor.fetchone()
         user = User(response[0], response[1], response[2])
         return user
 
     def get_all_users(self) -> list:
         users = list()
-        self.cursor.execute("""SELECT vkid, thkruhm, sendStatus FROM users;""")
+        self.cursor.execute("""SELECT vkid, school_group, sender_status FROM users;""")
         response = self.cursor.fetchall()
         for user in response:
             users.append(User(user[0], user[1], user[2]))
@@ -48,13 +48,13 @@ class UserRequest:
 
     def add_user(self, user: User):
         self.cursor.execute(
-            f"INSERT INTO users(vkid, thkruhm, sendStatus) VALUES ({user.vkid}, {user.group}, {user.sender_status})")
+            f"INSERT INTO users(vkid, school_group, sender_status) VALUES ({user.vkid}, {user.group}, {user.sender_status})")
         self.database.connection.commit()
 
     def update_user(self, user: User):
         try:
             self.cursor.execute(
-                f"UPDATE users SET thkruhm = {user.group}, sendStatus = {user.sender_status} WHERE vkid = {user.vkid}")
+                f"UPDATE users SET school_group = {user.group}, sender_status = {user.sender_status} WHERE vkid = {user.vkid}")
             self.database.connection.commit()
         except:
             self.add_user(user)
